2fa1ecdc4b3af05b9ecc3940ee46993a48296b29
edubonifs <eduardo.bonilla@solo.io>
2023-11-22 11:55:05 +0100

    added limits

--- terraform/result-deploy.tf
+++ terraform/result-deploy.tf
@@ -23,12 +23,22 @@ resource "kubernetes_deployment" "result" {
 
       spec {
         container {
-          image = "dockersamples/examplevotingapp_result"
+          image = "dockersamples/examplevotingapp_result@sha256:1e805ccebf14b5317a5e3bbea571aa5494ced8eca8a77ebd12af9f9b62d59eb2"
           name  = "result"
 
           port {
             container_port = 80
             name           = "result"
+          }
+          resources {
+            requests = {
+              cpu    = "250m"
+              memory = "64Mi"
+            }
+            limits = {
+              cpu    = "500m"
+              memory = "128Mi"
+            }
           }
         }
       }
